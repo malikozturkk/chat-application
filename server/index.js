976a86a263d6bcb9956b7bf433fe004eba3521a5
malik.ozturk <malik.ozturk@enuygun.com>
2023-06-27 18:49:14 +0300

    feat: chatlist done

--- server/index.js
+++ server/index.js
@@ -14,20 +14,16 @@ const io = new Server(server, {
 })
 
 io.on("connection", (socket) => {
-    console.log(socket.id);
     socket.on("join_room", (data) => {
         socket.join(data)
-        console.log(`User id: ${socket.id} joined room : ${data}`)
     })
 
     socket.on("send_message", (data) => {
         socket.to(data.room).emit("receive_message", data)
     })
     socket.on("disconnect", () => {
-        console.log("user disconnected", socket.id)
     })
 })
 
 server.listen(3001, () => {
-    console.log("server running");
 });
